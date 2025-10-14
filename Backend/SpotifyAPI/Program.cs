83b7186a6f6c7a3f5d6d92f0cbbf1546e95262ec
marcsoler897 <marcsoler897@gmail.com>
2025-10-14 19:28:50 +0200

    a Program.cs canviat var's per WebApplicationBuilder/WebApplication/SQLConnection

--- Backend/SpotifyAPI/Program.cs
+++ Backend/SpotifyAPI/Program.cs
@@ -3,7 +3,7 @@ using SpotifyAPI.Services;
 using Microsoft.Data.SqlClient;
 using SpotifyAPI.EndPoints;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .SetBasePath(AppContext.BaseDirectory)
@@ -12,7 +12,7 @@ builder.Configuration
 string connectionString = builder.Configuration.GetConnectionString("SpotifyDBConnection") ?? "";
 SpotifyDBConnection dbConn = new SpotifyDBConnection(connectionString);
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 app.MapUserEndpoints(dbConn);
 
@@ -20,7 +20,7 @@ app.MapGet("/", () =>
 {
     try
     {
-        using var conn = dbConn.Create();
+        using SqlConnection conn = dbConn.Create();
         conn.Open();
         return $"Database connection: {conn.State}";
     }
