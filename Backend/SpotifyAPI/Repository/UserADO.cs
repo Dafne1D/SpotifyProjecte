b1f1c96153459d5c176c05608851231d777475ca
ElSuperMagikarp <uriens2006@gmail.com>
2025-10-15 17:31:34 +0200

    DBConnection canviat

--- Backend/SpotifyAPI/Repository/UserADO.cs
+++ Backend/SpotifyAPI/Repository/UserADO.cs
@@ -8,17 +8,22 @@ static class UserADO
 {
     public static void Insert(SpotifyDBConnection dbConn, User user)
     {
-        using var conn = dbConn.Create();
-        conn.Open();
-        var cmd = new SqlCommand("INSERT INTO Users (Id, Username, Email, Password, Salt, Hash) VALUES (@Id, @Username, @Email, @Password, @Salt, @Hash)", conn);
+        dbConn.Open();
+
+        string sql = @"INSERT INTO Users (Id, Username, Email, Password, Salt, Hash)
+                      VALUES (@Id, @Username, @Email, @Password, @Salt, @Hash)";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
         cmd.Parameters.AddWithValue("@Id", user.Id);
         cmd.Parameters.AddWithValue("@Username", user.Username);
         cmd.Parameters.AddWithValue("@Email", user.Email);
         cmd.Parameters.AddWithValue("@Password", user.Password);
         cmd.Parameters.AddWithValue("@Salt", user.Salt);
         cmd.Parameters.AddWithValue("@Hash", user.Hash);
+
         int rows = cmd.ExecuteNonQuery();
         Console.WriteLine($"{rows} fila inserida.");
-        conn.Close();
+
+        dbConn.Close();
     }
 }
--- Backend/SpotifyAPI/Services/SpotifyDBConnection.cs
+++ Backend/SpotifyAPI/Services/SpotifyDBConnection.cs
@@ -5,7 +5,28 @@ namespace SpotifyAPI.Services
     public class SpotifyDBConnection
     {
         private readonly string _connectionString;
-        public SpotifyDBConnection(string connectionString) { _connectionString = connectionString; }
-        public SqlConnection Create() => new SqlConnection(_connectionString);
+        public SqlConnection? sqlConnection;
+        public SpotifyDBConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public bool Open()
+        {
+            sqlConnection = new SqlConnection(_connectionString);
+
+            try
+            {
+                sqlConnection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public void Close()
+        {
+            sqlConnection!.Close();
+        }
     }
 }
