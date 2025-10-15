fc71436f1a45a25822eab8b557a29e146381feb6
marcsoler897 <marcsoler897@gmail.com>
2025-10-15 20:41:15 +0200

    Afegit GenerateSalt d'1 caràcter

--- Backend/SpotifyAPI/Utils/Hash.cs
+++ Backend/SpotifyAPI/Utils/Hash.cs
@@ -5,10 +5,19 @@ namespace SpotifyAPI.Utils;
 
 public static class Hash
 {
-    public static string ComputeHash(string text)
+    public static string GenerateSalt(int length = 1)
+    {
+        using var rng = RandomNumberGenerator.Create();
+        byte[] saltBytes = new byte[length];
+        rng.GetBytes(saltBytes);
+        return Convert.ToHexString(saltBytes)[..1];
+    }
+
+
+    public static string ComputeHash(string text, string salt)
     {
         using SHA256 sha256 = SHA256.Create();
-        byte[] bytes = Encoding.UTF8.GetBytes(text);
+        byte[] bytes = Encoding.UTF8.GetBytes(text + salt);
         byte[] hashBytes = sha256.ComputeHash(bytes);
         StringBuilder sb = new StringBuilder();
         foreach (byte b in hashBytes)
