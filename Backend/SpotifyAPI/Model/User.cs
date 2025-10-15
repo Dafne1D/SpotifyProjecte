37d08777551fb735c885eec765017e4e2cc67e52
marcsoler897 <marcsoler897@gmail.com>
2025-10-15 20:33:42 +0200

    Tret Hash d'User.cs

--- Backend/SpotifyAPI/Model/User.cs
+++ Backend/SpotifyAPI/Model/User.cs
@@ -7,5 +7,4 @@ public class User
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
     public string Salt { get; set; } = "";
-    public string Hash { get; set; } = "";
 }
