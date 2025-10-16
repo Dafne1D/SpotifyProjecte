28fc1cb0c972b9d42bff59c60b7505cd8ff645cb
marcsoler897 <marcsoler897@gmail.com>
2025-10-16 16:29:16 +0200

    Afegit delete where Id= a PlaylistADO.cs

--- Backend/SpotifyAPI/Repository/PlaylistADO.cs
+++ Backend/SpotifyAPI/Repository/PlaylistADO.cs
@@ -101,4 +101,20 @@ static class PlaylistADO
         dbConn.Close();
     }
 
+    public static bool Delete(SpotifyDBConnection dbConn, Guid id)
+    {
+        dbConn.Open();
+
+        string sql = @"DELETE FROM Playlists WHERE Id = @Id";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", id);
+
+        int rows = cmd.ExecuteNonQuery();
+
+        dbConn.Close();
+
+        return rows > 0;
+    }
+
 }
