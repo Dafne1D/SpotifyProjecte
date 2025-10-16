d17d4dcbb3b6984630889c5501916a64b677d216
ElSuperMagikarp <uriens2006@gmail.com>
2025-10-16 16:14:18 +0200

    Funcions de fitxers mogudes a la seva propia classe a utils

--- Backend/SpotifyAPI/EndPoints/Song.cs
+++ Backend/SpotifyAPI/EndPoints/Song.cs
@@ -1,7 +1,10 @@
-using SpotifyAPI.Repository;
-using SpotifyAPI.Services;
-using SpotifyAPI.Model;
 using Microsoft.AspNetCore.Mvc;
+
+using SpotifyAPI.Repository;
+using SpotifyAPI.Model;
+using SpotifyAPI.Services;
+using SpotifyAPI.Utils;
+
 
 namespace SpotifyAPI.EndPoints;
 
@@ -84,50 +87,12 @@ public static class SongEndpoints
             if (song is null)
                 return Results.NotFound(new { message = $"Song with Id {id} not found." });
 
-            InsertFiles(dbConn, id, files);
+            FileHandler.InsertFiles(dbConn, id, files);
 
             return Results.Ok(new { message = "Files successfully uploaded", files });
         })
         .Accepts<IFormFile[]>("multipart/form-data")
         .DisableAntiforgery();
-    }
-
-    public static async void InsertFiles(SpotifyDBConnection dbConn, Guid id, List<IFormFile> files)
-    {
-        foreach (IFormFile file in files)
-        {
-            Console.WriteLine($"PROCESSING FILE {file.Name}");
-            string filePath = await SaveFile(id, file);
-
-            SongFile songFile = new SongFile
-            {
-                Id = Guid.NewGuid(),
-                SongId = id,
-                Url = filePath
-            };
-
-            SongFileADO.Insert(dbConn, songFile);
-
-            Console.WriteLine($"FILE {file.Name} FINISHED PROCESSING");
-        }
-    }
-
-    public static async Task<string> SaveFile(Guid id, IFormFile file)
-    {
-        string uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
-
-        if (!Directory.Exists(uploadsFolder))
-            Directory.CreateDirectory(uploadsFolder);
-
-        string fileName = $"{id}_{Path.GetFileName(file.FileName)}";
-        string filePath = Path.Combine(uploadsFolder, fileName);
-
-        using (FileStream stream = new FileStream(filePath, FileMode.Create))
-        {
-            await file.CopyToAsync(stream);
-        }
-
-        return filePath;
     }
 }
 
