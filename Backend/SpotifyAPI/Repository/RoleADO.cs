91ae6fed519b21c61ccdab7406bcef14f2d1eb97
ElSuperMagikarp <uriens2006@gmail.com>
2025-10-28 18:20:54 +0100

    GetById afegit a RoleADO

--- Backend/SpotifyAPI/Repository/RoleADO.cs
+++ Backend/SpotifyAPI/Repository/RoleADO.cs
@@ -48,4 +48,29 @@ static class RoleADO
         dbConn.Close();
         return roles;
     }
+
+    public static Role? GetById(SpotifyDBConnection dbConn, Guid id)
+    {
+        dbConn.Open();
+        string sql = "SELECT Id, Name, Description FROM Roles WHERE Id = @Id";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", id);
+
+        using SqlDataReader reader = cmd.ExecuteReader();
+        Role? role = null;
+
+        if (reader.Read())
+        {
+            role = new Role
+            {
+                Id = reader.GetGuid(0),
+                Name = reader.GetString(1),
+                Description = reader.GetString(2)
+            };
+        }
+
+        dbConn.Close();
+        return role;
+    }
 }
