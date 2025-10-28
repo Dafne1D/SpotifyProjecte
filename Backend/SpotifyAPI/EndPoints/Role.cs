4945aefe39ec581d91a1def9c4bbc28b6a3aa2b2
ElSuperMagikarp <uriens2006@gmail.com>
2025-10-28 17:33:11 +0100

    Role Endpoints afegit amb Get All

---
+++ Backend/SpotifyAPI/EndPoints/Role.cs
@@ -0,0 +1,24 @@
+using SpotifyAPI.Services;
+using SpotifyAPI.Model;
+using SpotifyAPI.Repository;
+using SpotifyAPI.DTO;
+
+namespace SpotifyAPI.EndPoints;
+
+public static class RoleEndpoints
+{
+    public static void MapRoleEndpoints(this WebApplication app, SpotifyDBConnection dbConn)
+    {
+        // GET /roles
+        app.MapGet("/roles", () =>
+        {
+            List<Role> roles = RoleADO.GetAll(dbConn);
+            List<RoleResponse> roleResponses = new List<RoleResponse>();
+            foreach (Role role in roles)
+            {
+                roleResponses.Add(RoleResponse.FromRole(role));
+            }
+            return Results.Ok(roleResponses);
+        });
+    }
+}
