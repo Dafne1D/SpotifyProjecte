ef909dd3bf931badc2ef58c8cf55db63ff5aadba
ElSuperMagikarp <uriens2006@gmail.com>
2025-11-19 18:47:58 +0100

    CreateUserPage canviat a UpdateUserPage

---
+++ Frontend/AppSpotifyWPF/AppSpotifyWPF/Screens/Users/UpdateUserPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using AppSpotifyWPF.Classes;
+using AppSpotifyWPF.Services;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AppSpotifyWPF.Screens.Users
+{
+    public partial class NouCRUDUser : Page
+    {
+        private readonly ApiService _apiService = new ApiService();
+
+        public NouCRUDUser()
+        {
+            InitializeComponent();
+        }
+
+        private async void CreateUser_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string password = txtPassword.Password;
+            string repeatPassword = txtRepeatPassword.Password;
+
+            if (password != repeatPassword)
+            {
+                MessageBox.Show("The passwords do not match!");
+                return;
+            }
+
+            var newUser = new User
+            {
+                Username = name,
+                Email = email,
+                Rol
+            };
+
+            try
+            {
+                var createdUser = await _apiService.PostAsync<User>("/users", newUser);
+                MessageBox.Show($"User created! ID: {createdUser.Id}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error creating user: {ex.Message}");
+            }
+            ClearBoxes();
+        }
+
+        private void ClearBoxes()
+        {
+            txtName.Clear();
+            txtEmail.Clear();
+            txtPassword.Clear();
+            txtRepeatPassword.Clear();
+        }
+        private void BackToHome_Click(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+            if (parentWindow is HomeScreen home)
+            {
+                home.MainFrame.Visibility = Visibility.Collapsed;
+                home.HomeContent.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
