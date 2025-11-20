3802754a5e7048175460246677ec287fcd0109e3
ElSuperMagikarp <uriens2006@gmail.com>
2025-11-20 17:13:01 +0100

    ViewUserPage modificat perque coincideixi l'estil amb la resta de l'aplicació

--- Frontend/AppSpotifyWPF/AppSpotifyWPF/Screens/Users/ViewUserPage.xaml.cs
+++ Frontend/AppSpotifyWPF/AppSpotifyWPF/Screens/Users/ViewUserPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AppSpotifyWPF.Classes;
+using AppSpotifyWPF.Services;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -6,6 +7,7 @@ namespace AppSpotifyWPF.Screens.Users
 {
     public partial class ViewUserPage : Page
     {
+        private readonly ApiService _apiService = new ApiService();
         private User selectedUser;
 
         public ViewUserPage(User user)
@@ -22,13 +24,42 @@ namespace AppSpotifyWPF.Screens.Users
 
         private void BackToHome_Click(object sender, RoutedEventArgs e)
         {
-            //Window parentWindow = Window.GetWindow(this);
+            NavigationService.Navigate(new UserManagementPage());
+        }
 
-            //if (parentWindow is HomeScreen home)
-            //{
-            //    home.MainFrame.Visibility = Visibility.Collapsed;
-            //    home.HomeContent.Visibility = Visibility.Visible;
-            //}
+        private void editUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new UpdateUserPage(selectedUser));
+        }
+
+        private async void deleteUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedUser == null)
+            {
+                MessageBox.Show("No hi ha cap usuari seleccionat");
+                return;
+            }
+
+            var result = MessageBox.Show(
+                $"Estàs segur que vols eliminar \"{selectedUser.Username}\"?",
+                "Confirmació",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    await _apiService.DeleteAsync($"/users/{selectedUser.Id}");
+                    MessageBox.Show("Usuari eliminat");
+
+                    NavigationService.Navigate(new UserManagementPage());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error eliminant l'usuari:\n" + ex.Message);
+                }
+            }
         }
     }
 }
