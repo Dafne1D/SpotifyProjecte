07b3972ac029de88729b7ad43bfee8b8ae5dedd3
ElSuperMagikarp <uriens2006@gmail.com>
2025-12-20 19:54:53 +0100

    Imatge afegida a Model Playlist

--- Backend/SpotifyAPI/Model/Playlist.cs
+++ Backend/SpotifyAPI/Model/Playlist.cs
@@ -6,4 +6,5 @@ public class Playlist
     public Guid UserId { get; set; }
     public string Name { get; set; } = "";
     public string? Description { get; set; }
+    public string ImageUrl { get; set; } = "";
 }
