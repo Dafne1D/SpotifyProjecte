9c406bb682b66b5cb1abe78b5a51cc1b405db1b4
Marc Soler Martí <marcsoler897@gmail.com>
2026-01-13 15:54:14 +0100

    Refactor/Millora UserADOValidator.cs

--- Backend/SpotifyAPI/Validators/UserADOValidator.cs
+++ Backend/SpotifyAPI/Validators/UserADOValidator.cs
@@ -6,20 +6,40 @@ using SpotifyAPI.Services;
 using Microsoft.Data.SqlClient;
 using SpotifyAPI.DTO;
 
-
 namespace SpotifyAPI.Validators;
 
 public static class UserADOValidator
 {
+    private const string UsernameDuplicatedMessage = "Aquest nom d'usuari ja existeix";
+    private const string UsernameDuplicatedCode = "USERNAME_DUPLICAT";
+
+    private const string EmailDuplicatedMessage = "Aquest correu ja està registrat";
+    private const string EmailDuplicatedCode = "EMAIL_DUPLICAT";
+
     public static Result Validate(UserRequest user, SpotifyDBConnection dbConn)
     {
-        if (UserADO.UsernameExists(dbConn, user.Username))
-            return Result.Failure("Aquest nom d'usuari ja existeix", "USERNAME_DUPLICAT");
+        var result = ValidateUsername(user, dbConn);
+        if (!result.IsOk) return result;
 
+        result = ValidateEmail(user, dbConn);
+        if (!result.IsOk) return result;
+
+        return Result.Ok();
+    }
+
+    private static Result ValidateUsername(UserRequest user, SpotifyDBConnection dbConn)
+    {
+        if (UserADO.UsernameExists(dbConn, user.Username))
+            return Result.Failure(UsernameDuplicatedMessage, UsernameDuplicatedCode);
+
+        return Result.Ok();
+    }
+
+    private static Result ValidateEmail(UserRequest user, SpotifyDBConnection dbConn)
+    {
         if (UserADO.EmailExists(dbConn, user.Email))
-            return Result.Failure("Aquest correu ja està registrat", "EMAIL_DUPLICAT");
+            return Result.Failure(EmailDuplicatedMessage, EmailDuplicatedCode);
 
         return Result.Ok();
     }
 }
-
