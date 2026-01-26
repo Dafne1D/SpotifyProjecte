373f427665d20a5e05ad0be77510b46023358ff2
Marc Soler Martí <marcsoler897@gmail.com>
2026-01-26 18:54:32 +0100

    Afegit Roles.cs

---
+++ Backend/SpotifyAPI/Common/Roles.cs
@@ -0,0 +1,8 @@
+namespace SpotifyAPI.Common;
+
+public static class Roles
+{
+    public const string Listener = "Listener";
+    public const string Artist = "Artist";
+    public const string Admin = "Admin";
+}
