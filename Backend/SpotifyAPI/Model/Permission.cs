ec741f64821ac2d637ee778d9ad6bc6aa424fd2b
Marc Soler Martí <marcsoler897@gmail.com>
2026-01-26 19:02:57 +0100

    Aplicat canvis db.sql a Model Role/Permission.cs

--- Backend/SpotifyAPI/Model/Permission.cs
+++ Backend/SpotifyAPI/Model/Permission.cs
@@ -1,8 +1,7 @@
-namespace SpotifyAPI.Model;
-
 public class Permission
 {
     public Guid Id { get; set; }
+    public string Code { get; set; } = "";
     public string Name { get; set; } = "";
-    public string? Description { get; set; } = "";
+    public string Description { get; set; } = "";
 }
