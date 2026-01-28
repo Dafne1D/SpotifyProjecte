b2bf47e0a4430cd4a6495257e95549eed1a6ae3d
ElSuperMagikarp <uriens2006@gmail.com>
2026-01-29 00:24:58 +0100

    Spotify Sockets: Servidor i client provisional

---
+++ Backend/Spotify Song-viewer Server/Models/ClientInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Spotify_Song_viewer_Server.Models
+{
+    internal class ClientInfo
+    {
+        public string Username { get; }
+        public string Song { get; set; }
+
+        public ClientInfo(string username, string song)
+        {
+            Username = username;
+            Song = song;
+        }
+    }
+}
