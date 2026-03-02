f68e39e856d1f367215592650de9341db2f7fbe2
Marc Soler Martí <marcsoler897@gmail.com>
2026-03-02 18:59:52 +0100

    Afegit JWT, GET /jwt funciona

--- Backend/SpotifyAPI/Program.cs
+++ Backend/SpotifyAPI/Program.cs
@@ -3,8 +3,42 @@ using SpotifyAPI.Services;
 using Microsoft.Data.SqlClient;
 using SpotifyAPI.EndPoints;
 using SpotifyAPI.Utils;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddScoped<JswTokenService>();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration["Jwt:JwtSecretKey"]!)
+                ),
+
+            ValidateIssuer = true,
+            ValidIssuer = "demo",
+
+            ValidateAudience = true,
+            ValidAudience = "public",
+
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.FromSeconds(30)
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
 {
@@ -23,6 +57,8 @@ SpotifyDBConnection dbConn = new SpotifyDBConnection(connectionString);
 WebApplication SpotifyApp = builder.Build();
 
 SpotifyApp.UseCors("AllowReactApp");
+SpotifyApp.UseAuthentication();
+SpotifyApp.UseAuthorization();
 
 SpotifyApp.MapMethods("{*path}", new[] { "OPTIONS" }, () => Results.Ok())
    .RequireCors("AllowReactApp");
