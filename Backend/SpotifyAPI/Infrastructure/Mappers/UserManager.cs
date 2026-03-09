1dd922c9af02c6c6882db8e6b262f1fbfd28d539
Marc Soler Martí <marcsoler897@gmail.com>
2026-03-09 16:55:20 +0100

    Acabat UserMapper

--- Backend/SpotifyAPI/Infrastructure/Mappers/UserManager.cs
+++ Backend/SpotifyAPI/Infrastructure/Mappers/UserManager.cs
@@ -1,11 +1,36 @@
 using SpotifyAPI.Infrastructure.Persistence.Entities;
 using SpotifyAPI.Domain.Entities;
+using System.Runtime.CompilerServices;
+using System.Reflection.Metadata;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics.Contracts;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace dbdemo.Infrastructure.Mappers;
 
 public static class UserMapper
 {
 
+    public static User ToDomain(UserEntity userEntity)
+        => new User(
+            userEntity.Id,
+            userEntity.Username,
+            userEntity.Email,
+            userEntity.Password,
+            userEntity.Salt
+        );
+
+    public static UserEntity ToEntity(User user, Guid id)
+        => new User(
+            Id = id,
+            Username = user.Username,
+            Email = user.Email,
+            Password = user.Password,
+            Salt = user.Salt
+        );
 
         
 }
